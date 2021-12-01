28e473c0f50f28e21d0a6e738009df07b3b8ff40
riha7 <harik0725@gmail.com>
2021-12-01 19:15:22 +0530

    get data added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
         <h1>Main Page</h1>
         <h3 style={{color:'tomato'}}>Login page ready to create</h3>
         <h3 style={{color:'tomato'}}>Login page ready to create</h3>
+        <h2>Getdata added</h2>
       </header>
     </div>
   );
