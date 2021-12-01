ce4d00a119d22f2b255f54ba28e997bfd1315ef5
riha7 <harik0725@gmail.com>
2021-12-01 08:45:06 +0530

    alignitems added

--- src/GitPageBasic.js
+++ src/GitPageBasic.js
@@ -3,7 +3,7 @@ import React from 'react'
 const GitPageBasic = () => {
     return (
         <div>
-            <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',border:'2px solid tomato'}}>
+            <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',border:'2px solid tomato',alignItems:'center'}}>
             <h1>git page basic</h1>
             <p>i am in git project</p>
             </div>
