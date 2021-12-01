d2cdbb2d3b07bbeeece2d7198a7a8700355f2a8a
riha7 <harik0725@gmail.com>
2021-12-01 19:55:51 +0530

    new File created

---
+++ src/DataApintigration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DataApintigration = () => {
+    return (
+        <div>
+            <h1>API intigration start.......</h1>
+        </div>
+    )
+}
+
+export default DataApintigration
