ab49735561baeaf8618a0daa9c4a03a3bd1587c4
riha7 <harik0725@gmail.com>
2021-12-01 07:30:14 +0530

    GitBasic Page is modified

---
+++ src/GitPageBasic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GitPageBasic = () => {
+    return (
+        <div>
+            <h1>git page basic</h1>
+            <p>i am in git project</p>
+        </div>
+    )
+}
+
+export default GitPageBasic
