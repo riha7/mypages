b834a7f79a1646786c51cb6a48325d9a67e99f56
riha7 <harik0725@gmail.com>
2021-12-01 08:53:16 +0530

    table added

--- src/GitPageBasic.js
+++ src/GitPageBasic.js
@@ -6,6 +6,27 @@ const GitPageBasic = () => {
             <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',border:'2px solid tomato',alignItems:'center'}}>
             <h1>git page basic</h1>
             <p>i am in git project</p>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>s.no</th>
+                        <th>name</th>
+                        <th>item</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>hari</td>
+                        <td>idily</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>wada</td>
+                        <td>riha</td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
 
         </div>
