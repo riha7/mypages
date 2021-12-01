05cf802082c2f6f545faf4e9921b2c2a3ebd29be
riha7 <harik0725@gmail.com>
2021-12-01 08:36:16 +0530

    applied color in h3 element in app.js file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Main Page</h1>
-        <h3>Login page ready to create</h3>
+        <h3 style={{color:'tomato'}}>Login page ready to create</h3>
+
     <GitPageBasic />
       </header>
     </div>
