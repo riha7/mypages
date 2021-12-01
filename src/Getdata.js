d7e3e030056e5c0834e037a941f3bfd6dfa9cf0f
riha7 <harik0725@gmail.com>
2021-12-01 11:38:32 +0530

    api intigration Success

--- src/Getdata.js
+++ src/Getdata.js
@@ -1,30 +1,28 @@
-import axios from 'axios';
+import axios from 'axios'
 import React from 'react'
 
 const Getdata = () => {
-    const fetchData= async()=>{
-        const waitTime=5000;
-        setTimeout(()=>{
-            console.log('request to takiing long time');
-        },waitTime)
-        try {
-            const result = await axios({
-                url:"https://jsonplaceholder.typicode.com/todos/1",
-                headers:{
-                    accept:'application/json',
-                    "content-type":"application/json"
-                }
-            });
-            console.log("success!",JSON.stringify(result.data,null,2))
-        } catch (error) {
-            console.log("error displayed",error.message);
-        }
+    const fetchRequest=async()=>{
+        setTimeout(() => {
+            console.log("takiing long time to display....");
+        }, 5000);
+            try {
+                const result=await axios({
+                    url:"https://jsonplaceholder.typicode.com/todos/1",
+                    headers:{
+                        accept:"application/json",
+                        "content-type":"application/json"
+                    }
+                });
+                console.log('success!',JSON.stringify(result.data,null,2))
+            } catch (error) {
+                console.log("failure",error.message)
+            }
     }
     return (
         <div>
             <center>
-                <p>get Data</p>
-                <button onClick={()=>fetchData()}>Click to Get Data</button>
+                <button onClick={()=>fetchRequest()}>Click to gget data visible to console</button>
             </center>
         </div>
     )
