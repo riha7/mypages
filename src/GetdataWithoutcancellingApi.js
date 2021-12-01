261321c5129b4ff73a1da2dc040661787bc5ec1a
riha7 <harik0725@gmail.com>
2021-12-01 12:47:42 +0530

    color added

--- src/GetdataWithoutcancellingApi.js
+++ src/GetdataWithoutcancellingApi.js
@@ -3,7 +3,7 @@ import React from 'react'
 const GetdataWithoutcancellingApi = () => {
     return (
         <div>
-            <h1>get data from without cancelling Api</h1>
+            <h1 style={{color:'tomato'}}>get data from without cancelling Api</h1>
         </div>
     )
 }
