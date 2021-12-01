5f147a1666cfd43b88d9ce81ed807ca375476d62
riha7 <harik0725@gmail.com>
2021-12-01 08:40:55 +0530

    applied flex property

--- src/GitPageBasic.js
+++ src/GitPageBasic.js
@@ -3,8 +3,11 @@ import React from 'react'
 const GitPageBasic = () => {
     return (
         <div>
+            <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',border:'2px solid tomato'}}>
             <h1>git page basic</h1>
             <p>i am in git project</p>
+            </div>
+
         </div>
     )
 }
