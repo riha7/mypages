0ae4726f4cfdf7e9a522058acb6b4c94fe9988b0
riha7 <harik0725@gmail.com>
2021-12-01 13:46:29 +0530

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Getdata from './Getdata';
-import GitPageBasic from './GitPageBasic';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Main Page</h1>
         <h3 style={{color:'tomato'}}>Login page ready to create</h3>
-    <GitPageBasic />
-    <Getdata />
-    
       </header>
     </div>
   );
