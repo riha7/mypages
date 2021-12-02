2f27b9f103c3a5d884fd10d1a4ee274415295d3a
riha7 <harik0725@gmail.com>
2021-12-02 07:41:05 +0530

    error Modified

--- src/DisplayBrowser.js
+++ src/DisplayBrowser.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 //https://jsonplaceholder.typicode.com/users
 const DisplayBrowser = () => {
     const [data, setdata] = useState([])
