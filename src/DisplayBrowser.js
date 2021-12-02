0f26e18ca356260fef78e3bc58ca209c35c1446e
riha7 <harik0725@gmail.com>
2021-12-02 07:39:11 +0530

    display data to browser

--- src/DisplayBrowser.js
+++ src/DisplayBrowser.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 //https://jsonplaceholder.typicode.com/users
 const DisplayBrowser = () => {
+    const [data, setdata] = useState([])
+    useEffect(() => {
+        axios({
+            url:'https://jsonplaceholder.typicode.com/users',
+            headers:{
+                accept:'application/json',
+                "content-type":"application/json"
+            }
+        }).then(res=>{
+            console.log(res.data);
+            setdata(res.data);
+        })
+    }, [])
     return (
         <div>
-           <h2>Display Data to Browser</h2> 
+           <h2>Display Data to Browser</h2>
+           <table className="table">
+               <thead>
+                   <tr>
+                       <th>s.no</th>
+                       <th>username</th>
+                       <th>email</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   {
+                       data.map((item,index)=>{
+                           return(
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.username}</td>
+                                <td>{item.email}</td>
+                            </tr>
+                           )
+                       })
+                   }
+
+               </tbody>
+           </table> 
         </div>
     )
 }
